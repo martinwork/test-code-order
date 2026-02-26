e2b02363c8a64c559804f19e0a7d70c0be3af750
Martin Williams <martinwork@users.noreply.github.com>
2026-02-26 18:05:09 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,29 @@
+/**
+ * All
+ */
+function ALL_ON() {
+}
+function ALL_OFF() {
+}
+/**
+ * One
+ */
+function A_one() {
+}
+function B_one() {
+}
+function C_one() {
+}
+/**
+ * Two
+ */
+function A_two() {
+}
+function B_two() {
+}
+function C_two() {
+}
+
+basic.forever(function () {
+
+})
